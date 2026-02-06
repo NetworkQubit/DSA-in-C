e8c019917fba45387c2d4d7272aaeb1c58cc47ed
NetworkQubit <anuragrc117@gmail.com>
2026-02-07 00:10:12 +0530

    Add Linked List Code modification in the folder

--- Linked List/Doubly_Linked_List_Deletion.c
+++ Linked List/Doubly_Linked_List_Deletion.c
@@ -37,11 +37,88 @@ void display(){
     printf("\n");
 }
 
-void deleteatbeginning();
-void deleteatend();
-void deleteatposition(int position, int x);
-void deleteafterposition(int position, int x);
+void deletefrombeginning();
+void deletefromend();
+void deletefromposition(int position);
 
-void deleteatbeginning(){
+void deletefrombeginning(){
+    struct node* temp;
+    if (head == NULL){
+        printf("List is empty");
+    }else{
+    temp = head;
+    head = head->next;
+    head->prev = NULL;
+    free(temp);
+    }
+}
 
+void deletefromend(){
+    struct node* temp;
+    if (tail == NULL){
+        printf("List is empty");
+    }else{
+    temp = tail;
+    tail->prev->next = NULL;
+    tail = tail->prev;
+    free(temp);
+    }
+}
+
+void deletefromposition(int position){
+    struct node* temp;
+    int i = 1;
+    temp = head;
+    while (i < position){
+        temp = temp->next;
+        i++;
+    }
+    temp->prev->next = temp->next;
+    temp->next->prev = temp->prev;
+    free(temp);
+}
+
+int main(){
+    int choice;
+    int x, position;
+    while(1){
+    printf("1. Add Value\n2. Delete Value from Beginning\n3. Delete Value from End\n4. Delete Value from a Position\n5. Display\n6. Exit");
+    printf("\nEnter Your Choice: ");
+    scanf("%d", &choice);
+
+    switch(choice){
+
+    case 1:
+    printf("Enter Your Value: ");
+    scanf("%d",&x);
+    create(x);
+    break;
+
+    case 2:
+    deletefrombeginning();
+    break;
+
+    case 3:
+    deletefromend();
+    break;
+
+    case 4:
+    printf("Enter Your Position: ");
+    scanf("%d",&position);
+    deletefromposition(position);
+    break;
+
+    case 5:
+    display();
+    printf("\n");
+    break;
+
+    case 6:
+    exit(0);
+
+    default:
+    printf("Inavlid");
+    }
+}
+    return 0;
 }
